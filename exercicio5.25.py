2da914ad9873ebd3360983d92ed8cb83a9a64d10
JpSoares17 <jp.soares.monte@gmail.com>
2021-08-04 15:12:21 -0300

    Arquivo Novo

---
+++ exercicio5.25.py
@@ -0,0 +1,18 @@
+def main():
+    # Leitura:
+    numero = int(input("Digite um número: ").strip())
+    # Processamento:
+    base = 2
+    while True:
+        p = (base + (numero / base)) / 2
+        base = p
+        if p ** 2 - numero < 0.0001 and p ** 2 > numero:
+            break
+        if numero - p ** 2 < 0.0001 and numero > p ** 2:
+            break
+    # Saída:
+    print(p)
+
+
+if __name__ == "__main__":
+    main()
